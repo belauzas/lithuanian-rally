1b78096f3d504799c392cb315c5bb8100464b84a
Rimantas <belauzas@yahoo.com>
2019-11-08 14:44:29 +0200

    Speending

--- js/game.js
+++ js/game.js
@@ -3,6 +3,14 @@
 let start = null;
 let frame = 0;
 let turnSize = 1;       // %
+const minFrameTime = 50;
+const maxFrameTime = 1200;
+const maxSpeed = 240;
+const speedIncrement = (maxFrameTime - minFrameTime) / maxSpeed;
+let speed = 0;
+
+const roadLines = document.querySelectorAll('.road > .line');
+const speedometer = document.querySelector('.car-info > .speed > .value');
 
 const car = document.querySelector('.car');
 
@@ -25,6 +33,7 @@ window.requestAnimationFrame(step);
 window.addEventListener('keydown', (e) => {
     switch (e.keyCode) {
         case 38:        // top
+            updateSpeed(1);
             break;
 
         case 37:        // left
@@ -36,6 +45,7 @@ window.addEventListener('keydown', (e) => {
             break;
 
         case 40:        // bottom
+            updateSpeed(-1);
             break;
 
         default:
@@ -44,14 +54,38 @@ window.addEventListener('keydown', (e) => {
 })
 
 function makeTurn() {
-    console.log(data.position, data.turn);
     if ( data.turn !== 0 ) {
         const turn = data.position + data.turn;
-        if ( turn > 20 && turn < 60 ) {
+        if ( turn > 0 && turn < 80 ) {
             data.position = turn;
             car.style.left = data.position + '%';
         }
         data.turn = 0;
+    }   
+}
+
+function updateSpeed( increment ) {
+    // const minFrameTime = 50;
+    // const maxFrameTime = 1200;
+    // const frameTimeDiff = maxFrameTime - minFrameTime;
+    // const maxSpeed = 240;
+    // let speed = 0;
+
+    if ( (increment === -1 || increment === 1) &&
+         speed+increment >= 0 &&
+         speed+increment <= 240 ) {
+        speed += increment;
+
+        let frameTime = 0;
+        if ( speed !== 0 ) {
+            frameTime = maxFrameTime - speedIncrement * speed;
+        }
+
+        speedometer.textContent = speed;
+        for ( let i=0; i<4; i++ ) {
+            roadLines[i].style.animationDuration = frameTime + 'ms';
+        }
+
+        console.log(frameTime);
     }
-    
 }
